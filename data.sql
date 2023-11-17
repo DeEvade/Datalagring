f318257e84dba04939b6ab40b3893959521b75d6
Fredrik Kis <fredrik.kis03@gmail.com>
2023-11-17 11:19:04 +0100

    a

--- data.sql
+++ data.sql
@@ -1,3 +1,15 @@
+INSERT INTO instructor (first_name,last_name,address,social_security,email,phone)
+VALUES
+  ('Cheryl','Sexton','Ap #503-9312 Dolor Avenue',19673099082,'nibh.sit@hotmail.ca','1-248-825-0072'),
+  ('Orla','Fox','Ap #190-6484 Ligula. Street',26794555769,'cras.eget@protonmail.net','1-768-545-5782'),
+  ('Joshua','Kirk','402-2717 Vitae Ave',65531227785,'pede.nunc.sed@hotmail.edu','1-593-267-6936'),
+  ('Britanni','Hawkins','Ap #736-3679 Lectus St.',92682135763,'ullamcorper.duis@yahoo.com','1-313-661-0551'),
+  ('Mariam','Clay','Ap #453-2750 Velit. Av.',6342546396,'egestas@aol.edu','1-483-122-7457'),
+  ('Deirdre','Davenport','Ap #342-7665 Mattis Rd.',63301114713,'ornare.in@outlook.couk','1-477-777-5527'),
+  ('Astra','Foster','Ap #902-8529 Aliquet Rd.',46191796867,'consequat.enim@icloud.ca','1-644-246-1563'),
+  ('Reed','Coffey','Ap #366-7674 Aliquam, St.',89768078249,'egestas.lacinia@aol.net','1-830-325-0201'),
+  ('Mallory','Middleton','506-3768 Lorem Av.',27010474665,'ipsum.dolor@google.org','1-683-517-1713'),
+  ('Samantha','Mcknight','P.O. Box 409, 8719 A, Street',11717268689,'fermentum.metus.aenean@yahoo.edu','1-729-781-9578');
 
 INSERT INTO student (first_name,last_name,social_security,address,email,phone)
 VALUES
