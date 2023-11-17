c0e3f2977474d78a5373e9dfa10dbcf8e2252d72
Fredrik Kis <fredrik.kis03@gmail.com>
2023-11-17 09:03:52 +0100

    a

---
+++ data.sql
@@ -0,0 +1,24 @@
+INSERT INTO myTable (first_name,last_name,address,socail_security,email,phone)
+VALUES
+  ('Cheryl','Sexton','Ap #503-9312 Dolor Avenue',19673099082,'nibh.sit@hotmail.ca','1-248-825-0072'),
+  ('Orla','Fox','Ap #190-6484 Ligula. Street',26794555769,'cras.eget@protonmail.net','1-768-545-5782'),
+  ('Joshua','Kirk','402-2717 Vitae Ave',65531227785,'pede.nunc.sed@hotmail.edu','1-593-267-6936'),
+  ('Britanni','Hawkins','Ap #736-3679 Lectus St.',92682135763,'ullamcorper.duis@yahoo.com','1-313-661-0551'),
+  ('Mariam','Clay','Ap #453-2750 Velit. Av.',6342546396,'egestas@aol.edu','1-483-122-7457'),
+  ('Deirdre','Davenport','Ap #342-7665 Mattis Rd.',63301114713,'ornare.in@outlook.couk','1-477-777-5527'),
+  ('Astra','Foster','Ap #902-8529 Aliquet Rd.',46191796867,'consequat.enim@icloud.ca','1-644-246-1563'),
+  ('Reed','Coffey','Ap #366-7674 Aliquam, St.',89768078249,'egestas.lacinia@aol.net','1-830-325-0201'),
+  ('Mallory','Middleton','506-3768 Lorem Av.',27010474665,'ipsum.dolor@google.org','1-683-517-1713'),
+  ('Samantha','Mcknight','P.O. Box 409, 8719 A, Street',11717268689,'fermentum.metus.aenean@yahoo.edu','1-729-781-9578');
+INSERT INTO myTable (first_name,last_name,address,socail_security,email,phone)
+VALUES
+  ('Zahir','Welch','395-8997 Odio St.',43005647825,'rhoncus.nullam.velit@hotmail.net','1-253-873-2238'),
+  ('Kane','Frederick','Ap #775-6066 Aliquet Avenue',11987405146,'sed@google.edu','1-479-612-2895'),
+  ('Desiree','Johnson','204-5550 Nec St.',89504511167,'eget.metus.eu@hotmail.com','1-184-223-3455'),
+  ('Odysseus','Fletcher','971-9225 Tellus, Road',87012280771,'eu.erat@yahoo.net','1-551-235-4510'),
+  ('Amela','Cruz','Ap #362-1641 Risus St.',56355627423,'mi.pede@protonmail.couk','1-242-623-6202'),
+  ('Dale','Cooper','2014 Elit, Rd.',31500913116,'class.aptent@hotmail.ca','1-868-389-5137'),
+  ('Malcolm','Waters','599 Non Ave',41172810342,'nam@google.couk','1-523-717-4477'),
+  ('Tatiana','Santiago','Ap #164-3132 Aliquam Avenue',37288956657,'magna.malesuada@outlook.couk','1-214-837-3661'),
+  ('Jasper','Yang','488-7878 In Av.',89482186130,'felis.nulla@protonmail.com','1-384-339-1789'),
+  ('Sawyer','Pacheco','P.O. Box 439, 4390 Dui Ave',14099331130,'nunc.ullamcorper.eu@aol.ca','1-702-473-3804');
