75285ea359b3c70c5b1c3eecc2b61133b0b7ab8d
Fredrik Kis <fredrik.kis03@gmail.com>
2023-11-20 13:59:52 +0100

    Mer data har skrivits

--- data.sql
+++ data.sql
@@ -22,7 +22,7 @@ VALUES
   ('Kennedy','Burton',80665293954,'Ap #394-1400 Posuere Av.','ut@protonmail.edu','1-884-668-2695'),
   ('Chanda','Jarvis',84881159718,'584-7852 Mauris St.','pellentesque.tellus.sem@aol.net','1-268-754-4437'),
   ('Hanna','Day',139818375657,'759-4362 Interdum Avenue','integer.mollis@aol.ca','1-318-997-2869'),
-  ('Ava','Ortega',164156271173,'Ap #295-6681 Phasellus Ave','nulla@yahoo.edu','1-820-565-3770');
+  ('Ava','Ortega',164156271173,'Ap #295-6681 Phasellus Ave','nulla@yahoo.edu','1-820-565-3770'),
   ('Drake','Hutchinson',173394196955,'Ap #965-5298 Id, Av.','magna.sed.dui@google.couk','1-411-711-4747'),
   ('Armando','Robinson',21536733629,'Ap #905-7332 Sagittis Street','nunc.ut@yahoo.couk','1-536-237-4420'),
   ('Stone','Wise',117765911004,'P.O. Box 515, 9753 Ornare St.','mi.tempor@outlook.edu','1-258-750-6631'),
@@ -32,7 +32,7 @@ VALUES
   ('Alice','Neal',188174687209,'Ap #827-5530 Erat St.','ultricies.dignissim@yahoo.net','1-864-504-3825'),
   ('Flavia','Vance',43932033781,'6240 Suspendisse St.','nunc@google.ca','1-953-789-9172'),
   ('Brenda','Madden',156717491196,'Ap #718-3161 Egestas. Street','tempor.est.ac@protonmail.net','1-367-940-1125'),
-  ('Julie','Blankenship',80197157527,'312-4430 Donec Ave','vestibulum.accumsan@aol.net','1-676-675-3168');
+  ('Julie','Blankenship',80197157527,'312-4430 Donec Ave','vestibulum.accumsan@aol.net','1-676-675-3168'),
   ('Ora','Warren',15380924713,'P.O. Box 148, 6306 Venenatis St.','ornare.lectus.ante@outlook.couk','1-815-794-9697'),
   ('Shelby','Mendez',51293234964,'Ap #539-6364 Hendrerit Rd.','adipiscing.enim@protonmail.net','1-566-875-3746'),
   ('Justin','Webster',172126902479,'Ap #628-6203 Nullam Road','tellus.sem@aol.com','1-777-445-4611'),
@@ -42,7 +42,7 @@ VALUES
   ('Yoshi','Larson',137552494918,'9245 Dictum St.','et.magnis@yahoo.couk','1-534-761-1828'),
   ('Gisela','Blevins',154597024243,'Ap #345-3364 Elit. Rd.','amet.ornare@yahoo.edu','1-796-584-5958'),
   ('Daniel','Moody',162718907530,'Ap #901-6606 Sed Road','suspendisse.tristique@protonmail.org','1-315-634-2345'),
-  ('Samantha','Mercado',16387248166,'Ap #104-8113 Eget, Rd.','vel@aol.org','1-383-454-4147');
+  ('Samantha','Mercado',16387248166,'Ap #104-8113 Eget, Rd.','vel@aol.org','1-383-454-4147'),
   ('Axel','Hunt',109986574246,'596 Amet, Avenue','phasellus@google.com','1-210-655-1587'),
   ('Madonna','Lyons',132824306395,'P.O. Box 584, 291 Dolor. Avenue','a.mi.fringilla@google.couk','1-397-968-1161'),
   ('Chaim','Hoover',151296112629,'376-1185 Enim. Rd.','orci.in.consequat@protonmail.org','1-205-558-0650'),
@@ -52,7 +52,7 @@ VALUES
   ('Graham','Mayo',27939181833,'Ap #816-2802 Consectetuer Avenue','sagittis.duis@icloud.edu','1-202-337-7663'),
   ('Lillith','Cantu',89729070011,'P.O. Box 926, 7484 Nonummy Street','dapibus.quam.quis@icloud.net','1-127-782-5102'),
   ('Keith','Herring',20184261426,'P.O. Box 825, 182 Sed Ave','quam.a@icloud.net','1-835-536-7074'),
-  ('Zahir','Rivera',147001048099,'Ap #397-9797 Maecenas Road','nibh.dolor@icloud.net','1-548-443-4606');
+  ('Zahir','Rivera',147001048099,'Ap #397-9797 Maecenas Road','nibh.dolor@icloud.net','1-548-443-4606'),
   ('Tanya','Anderson',45848026809,'7063 Class Avenue','vulputate.velit@yahoo.ca','1-633-779-7519'),
   ('Kareem','Mcfarland',146415263222,'819-675 Fringilla Av.','ultricies@outlook.edu','1-664-848-6024'),
   ('Yasir','Lambert',58676452457,'5194 Rhoncus. St.','ut.molestie.in@google.com','1-836-524-6789'),
@@ -62,7 +62,7 @@ VALUES
   ('Daniel','Dale',10733521354,'328-3017 Est Street','lacus@yahoo.org','1-858-231-2238'),
   ('Fatima','Simon',100260083543,'5044 Proin Av.','sed.libero@google.org','1-891-405-5636'),
   ('Heidi','Floyd',13633384985,'Ap #394-5724 Nullam Street','netus.et.malesuada@icloud.org','1-365-651-4635'),
-  ('Sandra','Jensen',104537584345,'Ap #200-9663 Pede Rd.','iaculis.quis@aol.ca','1-445-117-7185');
+  ('Sandra','Jensen',104537584345,'Ap #200-9663 Pede Rd.','iaculis.quis@aol.ca','1-445-117-7185'),
   ('Brendan','Rutledge',94639704900,'Ap #316-6024 Elit. Road','mi.aliquam.gravida@hotmail.net','1-666-696-1271'),
   ('Fulton','Sears',151344901075,'862-7174 Libero Av.','mauris@aol.net','1-747-588-9011'),
   ('Doris','Charles',64213226239,'2508 Vel, St.','non.arcu.vivamus@google.ca','1-326-897-4826'),
@@ -72,7 +72,7 @@ VALUES
   ('Mallory','Chang',136872759330,'499-5268 Ut St.','nec.tempus.mauris@aol.edu','1-743-556-2441'),
   ('Elmo','Gill',126120578965,'267-3499 Mauris St.','gravida@hotmail.org','1-742-781-4618'),
   ('Adara','Sullivan',160444563908,'364-2217 Magna. Av.','turpis.nulla@protonmail.edu','1-754-328-2267'),
-  ('Dennis','Beard',62279585332,'521-5633 Malesuada St.','venenatis.vel@hotmail.couk','1-518-832-3115');
+  ('Dennis','Beard',62279585332,'521-5633 Malesuada St.','venenatis.vel@hotmail.couk','1-518-832-3115'),
   ('Nathaniel','Newman',72828480584,'Ap #587-6822 Enim. Avenue','proin@outlook.edu','1-804-887-2774'),
   ('Dai','Leon',149810592579,'3651 Elit. Ave','metus.in.lorem@outlook.com','1-335-241-7994'),
   ('Aidan','Reyes',99326315322,'804-6224 Sapien, Avenue','tellus.aenean.egestas@icloud.net','1-836-874-1612'),
@@ -82,7 +82,7 @@ VALUES
   ('Caldwell','Jennings',95109957686,'Ap #714-3750 Turpis Avenue','turpis.nec.mauris@hotmail.com','1-498-642-8125'),
   ('Violet','Nieves',51225342821,'699-6418 Sapien, Ave','neque.tellus.imperdiet@hotmail.edu','1-765-858-8158'),
   ('Rae','Small',110803740282,'Ap #498-1632 Magna Ave','donec.nibh.quisque@yahoo.couk','1-580-201-7813'),
-  ('Willa','Chandler',12553688542,'297-4900 Vel, Road','mollis.duis.sit@protonmail.edu','1-719-148-3957');
+  ('Willa','Chandler',12553688542,'297-4900 Vel, Road','mollis.duis.sit@protonmail.edu','1-719-148-3957'),
   ('Elvis','Mason',67151660490,'306-8990 Proin Rd.','elit.dictum@google.edu','1-823-709-3241'),
   ('Isaac','Carson',189883845198,'405-4938 Rutrum St.','vulputate.ullamcorper@outlook.org','1-576-342-4701'),
   ('Gary','Cooper',187764342752,'Ap #203-7981 Praesent St.','curabitur.egestas@protonmail.couk','1-651-674-1886'),
@@ -92,7 +92,7 @@ VALUES
   ('Bruce','Raymond',147281093453,'Ap #861-9024 Nulla Ave','nonummy.ultricies.ornare@icloud.ca','1-418-320-7161'),
   ('Conan','Salazar',102962450841,'P.O. Box 878, 6966 Risus, Avenue','fringilla.mi.lacinia@protonmail.edu','1-479-215-1131'),
   ('Armando','Burke',181643625331,'338-3904 Id, Rd.','sed.pede@outlook.couk','1-826-246-7593'),
-  ('Steel','Moran',173848312143,'P.O. Box 191, 2208 Egestas. Avenue','mi.fringilla@outlook.edu','1-102-568-2680');
+  ('Steel','Moran',173848312143,'P.O. Box 191, 2208 Egestas. Avenue','mi.fringilla@outlook.edu','1-102-568-2680'),
   ('Jaime','Barrera',94837053063,'Ap #212-956 Pede, Av.','ut.mi.duis@aol.ca','1-467-920-6249'),
   ('Chloe','Kaufman',105500367368,'Ap #847-9132 Nulla Road','orci.ut.semper@hotmail.net','1-345-797-7782'),
   ('Wing','Williams',99958813341,'Ap #896-6779 Mi Rd.','congue@icloud.net','1-418-226-6152'),
@@ -102,7 +102,7 @@ VALUES
   ('Rhonda','Prince',117633986403,'543-8086 Nunc Av.','vulputate.lacus@outlook.edu','1-685-578-7552'),
   ('Noah','Wheeler',25687152472,'P.O. Box 828, 6640 Ac, Ave','in.condimentum@yahoo.couk','1-162-112-5440'),
   ('Thaddeus','Hobbs',30093754918,'Ap #201-9584 Lacus. Avenue','consequat.nec@outlook.couk','1-217-221-1211'),
-  ('Wyoming','Vinson',127959719829,'3549 A, St.','ut.erat@protonmail.com','1-404-231-2534');
+  ('Wyoming','Vinson',127959719829,'3549 A, St.','ut.erat@protonmail.com','1-404-231-2534'),
   ('Rinah','Duran',71906419962,'176-4877 Proin Avenue','commodo.at@yahoo.net','1-836-720-8456'),
   ('Keith','Sweet',80786242399,'P.O. Box 459, 8577 Parturient Avenue','vulputate@icloud.org','1-572-975-9666'),
   ('Rooney','Hunter',184690090178,'Ap #965-5532 Egestas Av.','eleifend@aol.ca','1-661-398-4301'),
@@ -113,3 +113,43 @@ VALUES
   ('Doris','Mcintyre',122306880883,'558-2777 Gravida. Road','et.ultrices@yahoo.couk','1-432-547-0770'),
   ('Salvador','Copeland',130151921044,'Ap #460-7409 Pede. Street','per.inceptos@hotmail.com','1-331-384-1012'),
   ('Ima','Rios',191070814294,'307-8303 Luctus Av.','dignissim.magna@hotmail.org','1-593-577-3385');
+
+
+INSERT INTO time_slot (start_time, end_time)
+VALUES
+  ('2023-01-10 10:00:00', '2023-01-10 12:00:00'), -- First group lesson
+  ('2023-01-11 13:00:00', '2023-01-11 15:00:00'), -- Second group lesson
+  ('2023-01-10 10:00:00', '2023-01-10 12:00:00'), -- First ensemble lesson
+  ('2023-01-11 13:00:00', '2023-01-11 15:00:00'), -- Second ensemble lesson
+  ('2023-02-10 10:00:00', '2023-01-10 12:00:00'), -- First individual lesson
+  ('2023-02-10 10:00:00', '2023-01-10 12:00:00'); -- Second individual lesson
+
+insert into price('class_type', 'level', 'sibling_discount', 'cost')
+values 
+	('group', 'beginner', null, 200),
+	('group', 'intermediate', null, 400),
+	('group', 'advanced', null, 800),
+	('ensemble', 'beginner', null, 300),
+	('ensemble', 'intermediate', null, 600),
+	('ensemble', 'advanced', null, 900),
+	('individual', 'beginner', null, 500),
+	('individual', 'intermediate', null, 1000),
+	('individual', 'advanced', null, 2000);
+
+insert into instrument_type(name)
+values
+	('Piano'),
+	('Guitar'),
+	('Violin'),
+	('Drums');
+
+INSERT INTO group_lesson_student (group_lesson_id, student_id, price, state) 
+VALUES (
+    'group_lesson_id', -- Här måste det spcifika id ges
+    'studen_id', -- Den hör studenten vill anmäla sig till denna lektion
+    (SELECT cost
+     FROM price p
+     JOIN group_lesson gl ON p.id = gl.price_id
+     WHERE gl.id = '6eb25141-96ee-468f-bff4-b2d085126eca'),
+    'accepted'
+);
