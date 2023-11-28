8928963cc98a0fcbdbd4a1467461a0886182a15d
William Redgård <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>
2023-11-28 16:08:00 +0100

    -

--- backend/migrations/sqls/20231128095001-init-up.sql
+++ backend/migrations/sqls/20231128095001-init-up.sql
@@ -294,3 +294,43 @@ values
   ('00aec2fb-c652-42af-8ed1-272b6697afc3', '58d6fbee-722b-4a83-9f86-6e7b595bb7f8'), -- Dawn Huber and Lana Bonner
   ('58d6fbee-722b-4a83-9f86-6e7b595bb7f8', '00aec2fb-c652-42af-8ed1-272b6697afc3'); 
 
+INSERT INTO historical_lesson (lesson_type, genre, instrument, price, student_name, student_email)
+SELECT 
+    'Individual Lesson' AS lesson_type,
+    NULL AS genre,
+    it.name AS instrument,
+    ils.price AS price,
+    CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+    s.email AS student_email
+FROM individual_lesson_student ils
+JOIN individual_lesson il ON ils.individual_lesson_id = il.id
+JOIN instrument_type it ON il.instrument_type_name = it.name
+JOIN student s ON ils.student_id = s.id;
+
+
+INSERT INTO historical_lesson (lesson_type, genre, instrument, price, student_name, student_email)
+SELECT 
+    'Group Lesson',
+    NULL,  -- Replace with the actual genre value if applicable
+    it.name AS instrument,
+    gls.price AS price,
+    CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+    s.email AS student_email
+FROM group_lesson_student gls
+JOIN group_lesson gl ON gls.group_lesson_id = gl.id
+JOIN instrument_type it ON gl.instrument_type_name = it.name
+JOIN student s ON gls.student_id = s.id;
+
+
+INSERT INTO historical_lesson (lesson_type, genre, instrument, price, student_name, student_email)
+SELECT 
+    'Ensemble Lesson',
+    el.genre AS genre,
+    NULL AS instrument,
+    els.price AS price,
+    CONCAT(s.first_name, ' ', s.last_name) AS student_name,
+    s.email AS student_email
+FROM ensemble_lesson_student els
+JOIN ensemble_lesson el ON els.ensemble_lesson_id = el.id
+
+JOIN student s ON els.student_id = s.id;
