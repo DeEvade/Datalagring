5b9568db2c251680da3f2376c7ae3ed6108e1bdc
Fredrik Kis <fredrik.kis03@gmail.com>
2023-12-01 09:23:37 +0100

    ada

--- data.sql
+++ data.sql
@@ -308,6 +308,8 @@ FROM individual_lesson_student ils
 JOIN individual_lesson il ON ils.individual_lesson_id = il.id
 JOIN instrument_type it ON il.instrument_type_name = it.name
 JOIN student s ON ils.student_id = s.id
+JOIN time_slot ts on ts.id = il.time_slot_id 
+where NOW() > ts.start_time
 
 UNION
 
@@ -322,18 +324,20 @@ FROM group_lesson_student gls
 JOIN group_lesson gl ON gls.group_lesson_id = gl.id
 JOIN instrument_type it ON gl.instrument_type_name = it.name
 JOIN student s ON gls.student_id = s.id
+JOIN time_slot ts on ts.id = gl.time_slot_id 
+where NOW() > ts.start_time
 
 UNION
 
 SELECT 
     'Ensemble Lesson' AS lesson_type,
     el.genre AS genre,
-    it.name AS instrument,
+    NULL AS instrument,
     els.price AS price,
     CONCAT(s.first_name, ' ', s.last_name) AS student_name,
     s.email AS student_email
 FROM ensemble_lesson_student els
 JOIN ensemble_lesson el ON els.ensemble_lesson_id = el.id
-JOIN ensemble_lesson_instrument eli ON el.id = eli.ensemble_lesson_id
-JOIN instrument_type it ON eli.instrument_type_name = it.name
-JOIN student s ON els.student_id = s.id;
+JOIN student s ON els.student_id = s.id
+JOIN time_slot ts on ts.id = el.time_slot_id 
+where NOW() > ts.start_time;
--- historical.sql
+++ historical.sql
@@ -9,13 +9,15 @@ SELECT
 FROM individual_lesson_student ils
 JOIN individual_lesson il ON ils.individual_lesson_id = il.id
 JOIN instrument_type it ON il.instrument_type_name = it.name
-JOIN student s ON ils.student_id = s.id;
+JOIN student s ON ils.student_id = s.id
+JOIN time_slot ts on ts.id = il.time_slot_id 
+where NOW() > ts.start_time
 
+UNION
 
-INSERT INTO historical_lesson (lesson_type, genre, instrument, price, student_name, student_email)
 SELECT 
-    'Group Lesson',
-    NULL,  -- Replace with the actual genre value if applicable
+    'Group Lesson' AS lesson_type,
+    NULL AS genre,
     it.name AS instrument,
     gls.price AS price,
     CONCAT(s.first_name, ' ', s.last_name) AS student_name,
@@ -23,12 +25,14 @@ SELECT
 FROM group_lesson_student gls
 JOIN group_lesson gl ON gls.group_lesson_id = gl.id
 JOIN instrument_type it ON gl.instrument_type_name = it.name
-JOIN student s ON gls.student_id = s.id;
+JOIN student s ON gls.student_id = s.id
+JOIN time_slot ts on ts.id = gl.time_slot_id 
+where NOW() > ts.start_time
 
+UNION
 
-INSERT INTO historical_lesson (lesson_type, genre, instrument, price, student_name, student_email)
 SELECT 
-    'Ensemble Lesson',
+    'Ensemble Lesson' AS lesson_type,
     el.genre AS genre,
     NULL AS instrument,
     els.price AS price,
@@ -36,5 +40,6 @@ SELECT
     s.email AS student_email
 FROM ensemble_lesson_student els
 JOIN ensemble_lesson el ON els.ensemble_lesson_id = el.id
-
-JOIN student s ON els.student_id = s.id;
+JOIN student s ON els.student_id = s.id
+JOIN time_slot ts on ts.id = el.time_slot_id 
+where NOW() > ts.start_time;
